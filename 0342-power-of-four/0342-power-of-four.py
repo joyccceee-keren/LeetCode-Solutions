9d09d2a3aac46f0012b96dd79634f6dadb16c30d
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-13 18:16:43 +0530

    Time: 0 ms (100%), Space: 19.2 MB (58.04%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,13 +1,13 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         #base case
-        if(n<=0):
+        if n<=0:
             return False
-        if n == 1:
+        if n==1:
             return True
-        if n % 4 != 0:
+        if n%4!=0:
             return False
 
-            #recursive case
-        return self.isPowerOfFour(n//4)            
-        
+        #recursive case
+        return self.isPowerOfFour(n//4)           
+       
