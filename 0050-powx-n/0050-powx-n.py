eccdf1b4cd4f26ada882ae03edf59d0ab65055d2
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-14 17:38:56 +0530

    Time: 0 ms (100%), Space: 19.5 MB (55.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,6 +1,6 @@
 class Solution:
     def findPow(self, x,n):
-        # base case
+        #base case
         if n==0:
             return 1
         #recursive case
@@ -8,13 +8,10 @@ class Solution:
         if n%2==0:
             return a*a
         else:
-            return a*a*x  
-
+            return a*a*x        
     def myPow(self, x: float, n: int) -> float:
         if n>=0:
             return self.findPow(x,n)
         else:
-            return 1/self.findPow(x,n*(-1))
+            return 1/self.findPow(x,n*(-1))    
 
-
-        
