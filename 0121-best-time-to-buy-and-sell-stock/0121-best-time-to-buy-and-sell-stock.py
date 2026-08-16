b6081505cf7437a6fd546effd04065d63e53f6ad
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-16 19:27:49 +0530

    Time: 44 ms (62.66%), Space: 28.5 MB (75.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,12 +5,13 @@ class Solution:
 
         for i in range(1,len(prices)):
             curr_profit = prices[i] - min_price 
-            if curr_profit>profit:
-                profit = curr_profit
+            if  curr_profit>profit:
+                profit =  curr_profit
             min_price = min(min_price,prices[i])
 
+        return profit     
+           
 
-        return profit    
 
 
 
