ccafa76011bc0df68e8bd5afc456b8d41a7c477f
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-26 17:41:15 +0530

    Time: 93 ms (24.93%), Space: 22.4 MB (25.13%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,15 +4,12 @@ class Solution:
 
         if n<=2:
             return n
-
         start = 1
+
         for i in range(2,n):
-            if nums[i] != nums[start-1]:
-               start += 1
-               nums[start] = nums[i]
+            if nums[i]!=nums[start-1]:
+                start+=1
+                nums[start] = nums[i]
 
-        return start+1    
-
+        return start+1            
        
-
-        
