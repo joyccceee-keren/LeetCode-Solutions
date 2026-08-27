6b531460279ab8097ef4426522d4ab39b7dda2ef
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-27 19:33:21 +0530

    Time: 0 ms (100%), Space: 20.7 MB (7.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        dict1={}
+        dict1 = {}
 
         for i in range(n):
             rem = target - nums[i]
@@ -9,6 +9,4 @@ class Solution:
                 return[dict1[rem],i]
 
             dict1[nums[i]]=i    
-
-
-      
+       
