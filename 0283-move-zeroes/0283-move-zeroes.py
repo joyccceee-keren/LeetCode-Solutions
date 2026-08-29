80a1e8e88efa9445d039c358c0f4bd265975e9fb
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-08-29 13:58:51 +0530

    Time: 3 ms (81.7%), Space: 20.4 MB (89.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,8 +6,10 @@ class Solution:
         l = 0
         for r in range(len(nums)):
             if nums[r]:
-            
-                nums[l], nums[r] = nums[r], nums[l]
+                temp = nums[r]
+                nums[r] = nums[l]
+                nums[l] = temp
                 l += 1
         return nums        
-        
+                
+       
