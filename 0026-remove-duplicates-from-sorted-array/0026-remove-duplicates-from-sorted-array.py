a9ca386b3870ea84152bbec1230c7885995bf6e4
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-09 20:33:02 +0530

    Time: 4 ms (22.16%), Space: 20.5 MB (79.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,8 +6,6 @@ class Solution:
             if nums[i] != nums[start]:
                 start += 1
                 nums[start] = nums[i]
-        return start+1        
+        return start+1       
 
-
-    
        
