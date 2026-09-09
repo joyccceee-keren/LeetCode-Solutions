191181c4af349e423f7c0dc4c7f6dc53a8c7a8d2
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-09 20:20:04 +0530

    Time: 2 ms (85.46%), Space: 20.6 MB (25.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -10,5 +10,7 @@ class Solution:
                 nums[r] = nums[l]
                 nums[l] = temp
                 l += 1
-        return nums        
-        
+        return l        
+
+                
+       
