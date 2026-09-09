41654c86b1276ce6e22507f57139690fdb68d4a4
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-09 19:33:22 +0530

    Time: 0 ms (100%), Space: 19.5 MB (24.01%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -4,10 +4,11 @@ class Solution:
         ans = []
 
         for i in candies:
-            if (i+extraCandies)>=maxCandies:
+            if(i+extraCandies)>=maxCandies:
                 ans.append(True)
             else:
                 ans.append(False)
         return ans            
-        
-        
+
+
+       
