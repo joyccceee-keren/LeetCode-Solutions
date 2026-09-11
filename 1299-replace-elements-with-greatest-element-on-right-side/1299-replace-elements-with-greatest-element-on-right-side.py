09fa0809d2bf09d1291cb144b3808f689df53d7f
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-11 14:37:49 +0530

    Time: 12 ms (93.24%), Space: 20.3 MB (99.2%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -4,15 +4,10 @@ class Solution:
         #reverse iteration
         # new max = max(oldmax, arr[i])
         rightMax = -1
-        for i in range(len(arr) -1 , -1 ,-1):
+        for i in range(len(arr) -1, -1, -1):
             newMax = max(rightMax, arr[i])
             arr[i] = rightMax
             rightMax = newMax
-        return arr    
-            
+        return arr
 
-            
-
-        
-
-        
+       
