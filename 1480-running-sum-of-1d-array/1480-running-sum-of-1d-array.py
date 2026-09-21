24e15c55485e70de61515ed639004846712aa202
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-21 16:38:20 +0530

    Time: 3 ms (9.99%), Space: 19.1 MB (99.55%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -3,10 +3,8 @@ class Solution:
         n = len(nums)
         ans = []
         ans.append(nums[0])
-
         for i in range(1,n):
             x = ans[i-1] + nums[i]
             ans.append(x)
-
         return ans    
        
