9558fe33261e1713e6db4a355a51e8a0366b607f
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-23 19:34:39 +0530

    Time: 32 ms (15.75%), Space: 39.2 MB (8%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,8 +5,6 @@ class Solution:
         for i in range(n):
             if nums[i] in dict1:
                 return True
-            dict1[nums[i]]= i
+            dict1[nums[i]] = i
         return False        
-                
-                
-       
+      
