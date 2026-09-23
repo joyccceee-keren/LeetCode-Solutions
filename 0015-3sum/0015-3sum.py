e5a4be7850179a8e977bab6c27bc5e6d71ed602b
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-23 15:41:35 +0530

    Time: 566 ms (76.3%), Space: 22.1 MB (94.48%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,7 +5,7 @@ class Solution:
         for i,a in enumerate(nums):
             if i>0 and a == nums[i-1]:
                 continue
-            l,r = i+1, len(nums)-1
+            l,r = i+1 , len(nums)-1
             while l<r:
                 threeSum = a + nums[l] + nums[r]
                 if threeSum > 0:
@@ -17,7 +17,8 @@ class Solution:
                     l += 1
                     while nums[l] == nums[l-1] and l<r:
                         l += 1
-        return ans                   
+        return ans                
+
 
 
        
