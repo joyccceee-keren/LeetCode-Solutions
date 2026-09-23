b14102e3de319050e71d38f3fd005a9ebff61b0e
Joyce Keren . K <kerenjoyce01@gmail.com>
2026-09-23 06:16:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (15.85%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -7,6 +7,5 @@ class Solution:
         for i in range(n):
             if nums[i] > nums[(i+1)%n]:
                 count += 1
-        return count<=1        
-
+        return count<=1            
        
